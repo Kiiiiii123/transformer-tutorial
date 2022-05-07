8442275314638ccac201b56f4524e5d1478066e4
Kiiiiii123 <zf1721418@buaa.edu>
2022-05-07 23:31:58 +0800

    Add the Attention Machanism function.

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from turtle import forward
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.autograd import Variable
 
 
@@ -59,3 +60,20 @@ def subsequent_mask(size):
     attn_shape = (1, size, size)
     subsequent_mask = np.triu(np.ones(attn_shape), k=1).astype("uint8")
     return torch.from_numpy(1 - subsequent_mask)
+  
+
+# Attention Machanism
+def attention(query, key, value, mask=None, dropout=None):
+    d_k = query.size(-1)
+    scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
+
+    if mask is not None:
+        scores = scores.masked_fill(mask == 0, -1e-9)
+
+    p_attn = F.softmax(scores, dim=-1)
+
+    if dropout is not None:
+        p_attn = dropout(p_attn)
+
+    return torch.matmul(p_attn, value), p_attn
+
