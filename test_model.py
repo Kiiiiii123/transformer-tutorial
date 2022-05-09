b8986db6e581b2495cb3f3aa25ba743e78218e00
Kiiiiii123 <zf1721418@buaa.edu>
2022-05-09 23:30:01 +0800

    Add the run function.

--- test_model.py
+++ test_model.py
@@ -1,14 +1,11 @@
-from pyitcast.transformer_utils import (
-    Batch,
-    LabelSmoothing,
-    SimpleLossCompute,
-    get_std_opt,
-)
+import numpy as np
+import torch
+from pyitcast.transformer_utils import (Batch, LabelSmoothing,
+                                        SimpleLossCompute, get_std_opt,
+                                        run_epoch)
+from torch.autograd import Variable
 
 from model import make_model
-import torch
-import numpy as np
-from torch.autograd import Variable
 
 
 def data_generator(V, batch, num_batch):
@@ -18,18 +15,44 @@ def data_generator(V, batch, num_batch):
         source = Variable(data, requires_grad=False)
         target = Variable(data, requires_grad=False)
 
-        yield Batch(source, target) # generator
+        yield Batch(source, target)  # generator
 
 
 V = 11
 batch = 20
 num_batch = 30
 
-if __name__ == "__main__":
-    res = data_generator(V, batch, num_batch)
-    print(res) # we will get a generator object
+# if __name__ == "__main__":
+#     res = data_generator(V, batch, num_batch)
+#     print(res) # we will get a generator object
 
 model = make_model(V, V, N=2)
 model_optimizer = get_std_opt(model)
 criterion = LabelSmoothing(size=V, padding_idx=0, smoothing=0.0)
 loss = SimpleLossCompute(model.generator, criterion, model_optimizer)
+
+# import pyplot as plt
+
+# crit = LabelSmoothing(size=5, padding_idx=0, smoothing=0.5)
+# predict = Variable(
+#     torch.LongTensor(
+#         [
+#             [0, 0.2, 0.7, 0.1, 0],
+#             [0, 0.2, 0.7, 0.1, 0],
+#             [0, 0.2, 0.7, 0.1, 0],
+#         ]
+#     )
+# )
+# target = Variable(torch.LongTensor([2, 1, 0]))
+# crit(predict, target)
+# plt.imshow(crit.true_dist)
+
+def run(model, loss, epochs=10):
+    for epoch in range(epochs):
+        model.train()
+        run_epoch(data_generator(V, 8, 20), model, loss)
+
+        model.eval()
+        run_epoch(data_generator(V, 8, 5), model, loss)
+
+epochs = 10
